37bb4bea6e106fa4e423f60329647399d6e561c7
JohnDorien1 <JohnDorien1@users.noreply.github.com>
2014-10-09 13:16:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -87,8 +87,8 @@ var reqGet = https.request(optionsgeta, function(res) {
                       rawbitstamp = d;
                       console.log('raw of stamp: ', rawbitstamp);
                       // Calc the averages and final price
-                      pricsysbittrex = rawbittrex.result[2];
-                      pricesyscryptsy = rawcryptsy.result.market.SYS.lasttradeprice;
+                      pricsysbittrex = rawbittrex.result['Last'];
+                      pricesyscryptsy = rawcryptsy.result['market']['SYS']['lasttradeprice'];
                       pricebtcbtce = rawbtce.btc_usd.last;
                       pricebtcbitstamp = rawbitstamp.last;
                       
